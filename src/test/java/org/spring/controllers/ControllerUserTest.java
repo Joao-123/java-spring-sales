170ce2c494386e3bf9be39933c03402e74ab0d31
RichardSeverich <richard.severich@gmail.com>
2019-06-26 17:48:33 -0700

    issue/002: Add test controller

--- src/test/java/org/spring/controllers/ControllerUserTest.java
+++ src/test/java/org/spring/controllers/ControllerUserTest.java
@@ -4,14 +4,23 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
+import org.spring.models.User;
 import org.spring.services.ServicesUser;
+import org.spring.support.MessageManager;
+import org.spring.support.Response;
+import org.spring.support.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 /**
@@ -34,22 +43,117 @@ public class ControllerUserTest {
     @MockBean
     private ServicesUser ServicesUserMocked;
 
-    @Test
-    public void simpleResult() throws Exception {
+    private Response<User> response;
 
-        // this is the expected JSON answer
-        String responseBody = "{\"content\":\"Hello World from Spring!\"}";
+    private ResponseBody<User> responseBody;
+
+    private List<User> listUsers;
+
+    /**
+     * Before.
+     */
+    @Before
+    public void setUp() {
+        response = new Response<>();
+        responseBody = new ResponseBody<>();
+        listUsers = new ArrayList<>();
+    }
+
+    /**
+     * Test 1.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test1() throws Exception {
+
+        // Build Response
+        User user = new User();
+        user.setId("5421857");
+        user.setPassword("pass123");
+        user.setName("Isac");
+        user.setLastName("Newton");
+        user.setBirthDate("26/06/1996");
+        user.setEmail("Isac.Newton@gmail.com");
+        user.setType("admin");
+        listUsers.add(user);
+        responseBody.setData(listUsers);
+        responseBody.setTextMessage(MessageManager.getSuccessfully("Users"));
+        response
+                .setHttpStatus(HttpStatus.OK)
+                .setBody(responseBody);
 
         // we set the result of the mocked service
-        //BDDMockito.given(ServicesUserMocked.add()),
+        //BDDMockito.given(ServicesUserMocked.add(user)).willReturn(new Response());
+        BDDMockito.when(ServicesUserMocked.getAll()).thenReturn(response);
+
+        // This is the JSON expected.
+        String expectedBody = "{\"data\":[{\"" +
+                "id\":\"5421857\",\"" +
+                "password\":\"pass123\",\"" +
+                "name\":\"Isac\",\"" +
+                "lastName\":\"Newton\",\"" +
+                "birthDate\":\"26/06/1996\",\"" +
+                "email\":\"Isac.Newton@gmail.com\",\"" +
+                "type\":\"admin\"}],\"" +
+                "textMessage\":\"The get Users  successfully\"}";
 
         // the test is executed:
         // perform: it executes the request and returns a ResultActions object
         // accept: type of media accepted as response
         // andExpect: ResultMatcher object that defines some expectations
-        this.mockMvc.perform(get("/simple")
-                .accept(MediaType.APPLICATION_JSON))
+        this.mockMvc
+                .perform(get("/api/v1/users"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(responseBody));
+                .andExpect(MockMvcResultMatchers.content().string(expectedBody));
+    }
+
+    /**
+     * Test 1.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test1() throws Exception {
+
+        // Build Response
+        User user = new User();
+        user.setId("5421857");
+        user.setPassword("pass123");
+        user.setName("Isac");
+        user.setLastName("Newton");
+        user.setBirthDate("26/06/1996");
+        user.setEmail("Isac.Newton@gmail.com");
+        user.setType("admin");
+        listUsers.add(user);
+        responseBody.setData(listUsers);
+        responseBody.setTextMessage(MessageManager.getSuccessfully("Users"));
+        response
+                .setHttpStatus(HttpStatus.OK)
+                .setBody(responseBody);
+
+        // we set the result of the mocked service
+        //BDDMockito.given(ServicesUserMocked.add(user)).willReturn(new Response());
+        BDDMockito.when(ServicesUserMocked.getAll()).thenReturn(response);
+
+        // This is the JSON expected.
+        String expectedBody = "{\"data\":[{\"" +
+                "id\":\"5421857\",\"" +
+                "password\":\"pass123\",\"" +
+                "name\":\"Isac\",\"" +
+                "lastName\":\"Newton\",\"" +
+                "birthDate\":\"26/06/1996\",\"" +
+                "email\":\"Isac.Newton@gmail.com\",\"" +
+                "type\":\"admin\"}],\"" +
+                "textMessage\":\"The get Users  successfully\"}";
+
+        // the test is executed:
+        // perform: it executes the request and returns a ResultActions object
+        // accept: type of media accepted as response
+        // andExpect: ResultMatcher object that defines some expectations
+        this.mockMvc
+                .perform(get("/api/v1/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedBody));
     }
 }
