3422446031787d13aac5f66c8253ecf3053fa54b
RichardSeverich <richard.severich@gmail.com>
2019-06-27 11:08:01 -0700

    issue/002: Add unit tests

--- src/test/java/org/spring/controllers/ControllerUserTest.java
+++ src/test/java/org/spring/controllers/ControllerUserTest.java
@@ -1,6 +1,6 @@
 package org.spring.controllers;
 
-import ch.qos.logback.core.net.ObjectWriter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,8 +23,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-
 
 /**
  * Test.
@@ -41,6 +39,9 @@ public class ControllerUserTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     // we mock the service, here we test only the controller
     // @MockBean is a Spring annotation that depends on mockito framework
     @MockBean
@@ -49,8 +50,6 @@ public class ControllerUserTest {
     private Response<User> response;
 
     private ResponseBody<User> responseBody;
-
-    private List<User> listUsers;
 
     private User user;
 
@@ -61,6 +60,7 @@ public class ControllerUserTest {
     public void setUp() {
         response = new Response<>();
         responseBody = new ResponseBody<>();
+        List<User> listUsers;
         listUsers = new ArrayList<>();
         // Build list of users
         user = new User();
@@ -73,7 +73,6 @@ public class ControllerUserTest {
         user.setType("admin");
         listUsers.add(user);
         responseBody.setData(listUsers);
-
     }
 
     /**
@@ -105,6 +104,7 @@ public class ControllerUserTest {
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/api/v1/users"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(jsonPath("$.id", "5421857"))
                 .andExpect(MockMvcResultMatchers.content().string(expectedBody));
     }
 
@@ -120,28 +120,27 @@ public class ControllerUserTest {
                 .setHttpStatus(HttpStatus.OK)
                 .setBody(responseBody);
         // Set the result of the mocked service
-        BDDMockito.when(ServicesUserMocked.getAll()).thenReturn(response);
-
+        BDDMockito.when(ServicesUserMocked.getById("5421857")).thenReturn(response);
         // This is the JSON expected.
-        String expectedBody = "{\"data\":[{\"" +
-                "id\":\"5421857\",\"" +
-                "password\":\"pass123\",\"" +
-                "name\":\"Isac\",\"" +
-                "lastName\":\"Newton\",\"" +
-                "birthDate\":\"26/06/1996\",\"" +
-                "email\":\"Isac.Newton@gmail.com\",\"" +
-                "type\":\"admin\"}],\"" +
-                "textMessage\":\"The get Users  successfully\"}";
-
         // Expect ResultMatcher object that was defined.
         this.mockMvc
                 .perform(MockMvcRequestBuilders.get("/api/v1/users/5421857"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(expectedBody));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].id").value("5421857"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].password").value("pass123"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].name").value("Isac"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].lastName").value("Newton"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].birthDate")
+                        .value("26/06/1996"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].email")
+                        .value("Isac.Newton@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].type").value("admin"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.textMessage")
+                        .value("The get Users  successfully"));
     }
 
     /**
-     * Test 3. Post Request.
+     * Test 3. Post.
      *
      * @throws Exception
      */
@@ -153,7 +152,7 @@ public class ControllerUserTest {
                 .setHttpStatus(HttpStatus.CREATED)
                 .setBody(responseBody);
         // we set the result of the mocked service
-        //BDDMockito.given(ServicesUserMocked.add(user)).willReturn(new Response());
+        //BDDMockito.given(ServicesUserMocked.add(user)).willReturn(response);
         BDDMockito.when(ServicesUserMocked.add(user)).thenReturn(response);
 
         // This is the JSON expected.
@@ -169,12 +168,11 @@ public class ControllerUserTest {
         // perform: it executes the request and returns a ResultActions object
         // accept: type of media accepted as response
         // andExpect: ResultMatcher object that defines some expectations
-
         this.mockMvc
                 .perform(MockMvcRequestBuilders
                         .post("/api/v1/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
+                        .content(objectMapper.writeValueAsString(this.user)))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.content().string(requestBody));
     }
